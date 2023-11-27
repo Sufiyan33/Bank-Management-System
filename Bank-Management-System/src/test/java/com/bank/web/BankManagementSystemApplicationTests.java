d721c3a5536506667334aabff031925323162ed5
Sufiyan33 <basu700736@gmail.com>
2023-11-27 14:52:29 +0530

    Creating a new project Bank Management System

---
+++ Bank-Management-System/src/test/java/com/bank/web/BankManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

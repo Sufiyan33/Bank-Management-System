c58cabad766898906fcdcd31a3a7ac7c38608327
Sufiyan33 <basu700736@gmail.com>
2023-11-30 19:07:43 +0530

    Created Skeleton for service layer interfaces

--- Bank-Management-System/src/main/java/com/bank/web/service/AccountService.java
+++ Bank-Management-System/src/main/java/com/bank/web/service/AccountService.java
@@ -1,5 +1,15 @@
 package com.bank.web.service;
 
-public class AccountService {
+import com.bank.web.entity.Account;
+import com.bank.web.entity.User;
 
+public interface AccountService {
+
+	public Account createAccount(User user);
+	public boolean isPinGenerated(String accountNumber);
+	public void generatePin(String accountNumber, String password, String pin);
+	public void updatePin(String accountNumber, String oldPin,String password, String newPin);
+	public void cashDeposit(String accountNumber, String pin, double amount);
+	public void cashWithdrawal(String accountNumber, String pin, double amount);
+	public void fundTransfer(String sourceAccounter, String targetAccountNumber, String pin, double amount);
 }
---
+++ Bank-Management-System/src/main/java/com/bank/web/service/UserService.java
@@ -0,0 +1,5 @@
+package com.bank.web.service;
+
+public interface UserService {
+
+}

5dc2fc397bc78713e1eb489f040e42f2bc163fe5
Sufiyan33 <basu700736@gmail.com>
2023-12-13 12:14:01 +0530

    Added api

--- Bank-Management-System/src/main/java/com/bank/web/controller/AccountController.java
+++ Bank-Management-System/src/main/java/com/bank/web/controller/AccountController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +40,7 @@ public class AccountController {
 	@Autowired
 	private AccountService accountService;
 	
+	@PostMapping("/pin/generate")
 	public ResponseEntity<?> createPin(@RequestBody PinRequest pinRequest){
 		accountService.generatePin(LoggedInUser.getAccountNumber(), pinRequest.getPassword(), pinRequest.getPin());
 		
